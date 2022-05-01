2c10fec91b40094f721b4d1f906e30cd3a5232fc
jollaman999 <admin@jollaman999.com>
2022-05-01 14:50:31 +0900

    데이터베이스 접속정보 암호화

--- backend/src/main/java/com/eta/space/video/VideoApplication.java
+++ backend/src/main/java/com/eta/space/video/VideoApplication.java
@@ -1,5 +1,7 @@
 package com.eta.space.video;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
